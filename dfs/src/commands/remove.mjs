ec6902f2ba1047a81e2759d18b2953d397ea906a
cristian <alexander85vega@gmail.com>
2025-07-10 20:53:57 -0300

    Remove command implementation

--- dfs/src/commands/remove.mjs
+++ dfs/src/commands/remove.mjs
@@ -1,4 +1,46 @@
-#!/usr/bin/env zx
-import { $ } from "zx";
-// import chalk from "chalk";
+import printHelp from "../utils/printHelp.mjs";
+import runCommand from "../utils/runCommand.mjs";
+import printSection from "../utils/printSection.mjs";
+import text from "../locales/es/remove.json" with { type: "json" };
+import {isFlatpakInstalled, isSnapInstalled} from "../utils/flatpakAndSnap.mjs";
 
+// --- Exported main function ---
+export default async function remove(commands, arg, option) {
+  console.log(arg);
+
+  const systemRemove = {
+    label: text.title1,
+    color: "yellow",
+    cmd: [commands.pack, commands.remove, arg, commands["-y"]],
+  };
+
+  const flatpakRemove = {
+    label: text.title1,
+    color: "blue",
+    cmd: ["pack", "remove", [arg], "-y"],
+    isInstalled: isFlatpakInstalled,
+  };
+
+  const snapRemove = {
+    label: text.title2,
+    color: "red",
+    cmd: ["pack", "remove", [arg], "-y"],
+    isInstalled: isSnapInstalled,
+  };
+
+  switch (option) {
+    case "-d":
+      await runCommand(systemRemove);
+      break;
+    case "-f":
+      await runCommand(flatpakRemove);
+      break;
+    case "-s":
+      await runCommand(snapRemove);
+      break;
+    default:
+      printSection("red", text.error);
+      printHelp(text.help);
+      break;
+  }
+}
